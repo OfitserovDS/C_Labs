fe4340d1d61c376423665a4a4cbb719cf765eff9
OfitserovDS <dima777ofitserov@gmail.com>
2024-11-12 16:18:33 +0300

    Add files via upload

--- SudokuRls.h
+++ SudokuRls.h
@@ -7,76 +7,98 @@
 #define st 1
 #define fn 9
 
+/*
 typedef char* str;
 typedef void (*gametable)(int, int);
+*/
 
+/*
 int isValid(int** arr, int rows, int cols, int row, int col, int num);
-void displayField(int** arr, int rows, int cols, int highlightRow, int highlightCol);
+void displayField(int** arr, int rows, int cols, int chosenRow, int chosenCol);
 void fillRandomCells(int** arr, int rows, int cols, int numFilled);
 int solveSudoku(int** arr, int rows, int cols);
-void userInput(int** arr, int rows, int cols);
+void userInput(int** arr, int rows, int cols); */
 
-
-
-int isValid(int** arr, int rows, int cols, int row, int col, int num) {
-    for (int i = 0; i < rows; ++i) {
-        if (*(*(arr + i) + col) == num) return 0;
+int isValid(int** arr, int size, int row, int col, int num) {
+    for (int i = 0; i < size; ++i) {
+        if (*(*(arr + i) + col) == num) 
+            return 0;
     }
-    for (int j = 0; j < cols; ++j) {
-        if (*(*(arr + row) + j) == num) return 0;
+    for (int j = 0; j < size; ++j) {
+        if (*(*(arr + row) + j) == num) 
+            return 0;
     }
-    int sqrtSize = (int)sqrt(rows);
-    int boxRow = row - row % sqrtSize;
-    int boxCol = col - col % sqrtSize;
+    int sqrtSize = (int)sqrt(size);
+    int boxRow = row - row % sqrtSize; //
+    int boxCol = col - col % sqrtSize; // можно упросить
     for (int i = 0; i < sqrtSize; i++) {
         for (int j = 0; j < sqrtSize; j++) {
-            if (*(*(arr + i + boxRow) + j + boxCol) == num) return 0;
+            if (*(*(arr + i + boxRow) + j + boxCol) == num) 
+                return 0;
         }
     }
     return 1;
 }
 
-void displayField(int** arr, int rows, int cols, int highlightRow, int highlightCol) {
+void displayField(int** arr, int size, int chosenRow, int chosenCol) {
     printf("\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            if (i == highlightRow && j == highlightCol) printf("[");
-            else printf(" ");
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (i == chosenRow && j == chosenCol) 
+                printf("[");
+            else 
+                printf(" ");
             
-            if (*(*(arr + i) + j) == 0) printf(".");
-            else printf("%d", *(*(arr + i) + j));
+            if (*(*(arr + i) + j) == 0) 
+                printf(".");
+            else 
+                printf("%d", *(*(arr + i) + j));
             
-            if (i == highlightRow && j == highlightCol) printf("]");
-            else printf(" ");
+            if (i == chosenRow && j == chosenCol) 
+                printf("]");
+            else 
+                printf(" ");
         }
         printf("\n");
     }
 }
 
-void fillRandomCells(int** arr, int rows, int cols, int numFilled) {
+void fillRandomCells(int** arr, int size, int numFilled) {
     int filled = 0;
     while (filled < numFilled) {
-        int row = rand() % rows;
-        int col = rand() % cols;
-        int num = (rand() % rows) + 1;
-        if (*(*(arr + row) + col) == 0 && isValid(arr, rows, cols, row, col, num)) {
+        int row = rand() % size;
+        int col = rand() % size;
+        int num = (rand() % size) + 1;
+        if (*(*(arr + row) + col) == 0 && isValid(arr, size, row, col, num)) {
             *(*(arr + row) + col) = num;
             filled++;
         }
     }
 }
 
-int solveSudoku(int** arr, int rows, int cols) {
-    for (int row = 0; row < rows; row++) {
-        for (int col = 0; col < cols; col++) {
+int isFilled(int** arr, int size){
+    for (int i=0;i<size;++i){
+        for (int j=0;j<size;++j){
+            if (*(*(arr+i)+j)==0)
+                return 0;
+        }
+    }
+    return 1;
+    
+}
+
+int solveSudoku(int** arr, int size) {
+    for (int row = 0; row < size; row++) {
+        for (int col = 0; col < size; col++) {
             if (*(*(arr + row) + col) == 0) {
-                for (int num = 1; num <= rows; num++) {
-                    if (isValid(arr, rows, cols, row, col, num)) {
+                for (int num = 1; num <= size; num++) {
+                    if (isValid(arr, size, row, col, num)) {
                         *(*(arr + row) + col) = num;
                         system("cls");
-                        displayField(arr, rows, cols, -1, -1);
-                        Sleep(500);
-                        if (solveSudoku(arr, rows, cols)) return 1;
+                        displayField(arr, size, -1, -1);
+                        Sleep(300);
+                        if (solveSudoku(arr, size)) 
+                            return 1;
                         *(*(arr + row) + col) = 0;
                     }
                 }
@@ -87,30 +109,43 @@ int solveSudoku(int** arr, int rows, int cols) {
     return 1;
 }
 
-void userInput(int** arr, int rows, int cols) {
+void userInput(int** arr, int size) {
     int currentRow = 0, currentCol = 0;
     int num;
     char command;
     while (1) {
+        if (isFilled(arr,size)==1){
+            system("cls");
+            displayField(arr,size,-1,-1);
+            printf("Win! Congratulations!!!");
+            break;
+        }
         system("cls");
-        displayField(arr, rows, cols, currentRow, currentCol);
+        displayField(arr, size, currentRow, currentCol);
         printf("\nUse WASD to move, E to enter number, Q to quit.\n");
         printf("Current cell: [%d][%d]\n", currentRow + 1, currentCol + 1);
-        command = getchar();
-        if (command == 'q' || command == 'Q') break;
-        if (command == 'w' && currentRow > 0) currentRow--;
-        if (command == 's' && currentRow < rows - 1) currentRow++;
-        if (command == 'a' && currentCol > 0) currentCol--;
-        if (command == 'd' && currentCol < cols - 1) currentCol++;
+        command = getch();
+        if (command == 'q' || command == 'Q') 
+            break;
+        if (command == 'w' && currentRow > 0) 
+            currentRow--;
+        if (command == 's' && currentRow < size - 1) 
+            currentRow++;
+        if (command == 'a' && currentCol > 0) 
+            currentCol--;
+        if (command == 'd' && currentCol < size - 1) 
+            currentCol++;
         if (command == 'e' || command == 'E') {
-            printf("Enter number (1-%d): ", rows);
+            printf("Enter number (1-%d): ", size);
             scanf("%d", &num);
-            if (num >= 1 && num <= rows && arr[currentRow][currentCol] == 0 && isValid(arr, rows, cols, currentRow, currentCol, num)) {
-                arr[currentRow][currentCol] = num;
+            if (num >= 1 && num <= size && *(*(arr+currentRow)+currentCol) == 0 && isValid(arr, size, currentRow, currentCol, num)) {
+                *(*(arr+currentRow)+currentCol)  = num;
             } else {
-                printf("Invalid number or cell already filled.\n");
+                printf("Invalid number or cell already filled.\n"); // solve the problem with changing user's inputs
                 Sleep(1000);
             }
         }
     }
 }
+
+
--- mainSudoku.c
+++ mainSudoku.c
@@ -1,34 +1,43 @@
 #include "SudokuRls.h"
 
+
 int main() {
     srand(time(0));
-    int rows, cols, op;
+    int size, op;
     int difficultyMode;
 
-    printf("Choose your game table: Type 1 - 4x4, 2 - 6x6, 3 - 9x9\n");
+    printf("Choose your game table: Type 1 - 4x4, 2 - 9x9\n");
     scanf("%d", &op);
-    if (op == 1) rows = cols = 4;
-    else if (op == 2) rows = cols = 6;
-    else if (op == 3) rows = cols = 9;
+    if (op == 1) size = 4;
+    else if (op == 2) size = 9;
 
-    int** arr = (int**)calloc(rows, sizeof(int*));
-    for (int i = 0; i < rows; ++i) arr[i] = (int*)calloc(cols, sizeof(int));
+    int** arr = (int**)calloc(size, sizeof(int*));
+    for (int i = 0; i < size; ++i) arr[i] = (int*)calloc(size, sizeof(int));
 
     printf("Choose the difficulty mode: 1 - Nightmare, 2 - Hard, 3 - Easy\n");
     scanf("%d", &difficultyMode);
 
-    fillRandomCells(arr, rows, cols, rows * difficultyMode);
+    fillRandomCells(arr, size, size * difficultyMode);
 
     int mode;
     printf("Choose the game mode:\n1. User's mode \n2. Auto solve\n");
     scanf("%d", &mode);
-    if (mode == 1) userInput(arr, rows, cols);
-    else if (mode == 2) {
-        if (!solveSudoku(arr, rows, cols)) printf("Sudoku puzzle is unsolvable.\n");
-        else displayField(arr, rows, cols, -1, -1);
+    if (mode == 1) {
+        userInput(arr, size);
+        
     }
 
-    for (int i = 0; i < rows; ++i) free(*(arr + i));
+    else if (mode == 2) {
+        if (!solveSudoku(arr, size)) 
+            printf("Sudoku puzzle is unsolvable.\n");
+        else {
+            displayField(arr, size, -1, -1);
+            printf("Easy for computer!");
+        }
+    }
+
+    for (int i = 0; i < size; ++i) 
+        free(*(arr + i));
     free(arr);
     return 0;
 }
