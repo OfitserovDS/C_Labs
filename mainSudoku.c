8a6c4a20783c038440d13917f3a953ebcebcbd69
OfitserovDS <dima777ofitserov@gmail.com>
2024-11-12 16:40:27 +0300

    Update mainSudoku.c

--- mainSudoku.c
+++ mainSudoku.c
@@ -1,3 +1,4 @@
+// Import library
 #include "SudokuRls.h"
 
 
@@ -5,30 +6,32 @@ int main() {
     srand(time(0));
     int size, op;
     int difficultyMode;
-
+    int mode;
+    
     printf("Choose your game table: Type 1 - 4x4, 2 - 9x9\n");
     scanf("%d", &op);
     if (op == 1) size = 4;
     else if (op == 2) size = 9;
 
+    // Allocating memory
     int** arr = (int**)calloc(size, sizeof(int*));
     for (int i = 0; i < size; ++i) arr[i] = (int*)calloc(size, sizeof(int));
 
     printf("Choose the difficulty mode: 1 - Nightmare, 2 - Hard, 3 - Easy\n");
     scanf("%d", &difficultyMode);
 
+    // Function call to fill first cells on gametable
     fillRandomCells(arr, size, size * difficultyMode);
 
-    int mode;
+    
     printf("Choose the game mode:\n1. User's mode \n2. Auto solve\n");
     scanf("%d", &mode);
     if (mode == 1) {
-        userInput(arr, size);
-        
+        userInput(arr, size); // Function call to start user's mode
     }
 
     else if (mode == 2) {
-        if (!solveSudoku(arr, size)) 
+        if (!solveSudoku(arr, size)) // Autosolver tricky call 
             printf("Sudoku puzzle is unsolvable.\n");
         else {
             displayField(arr, size, -1, -1);
@@ -36,6 +39,7 @@ int main() {
         }
     }
 
+    // Freeing up memory
     for (int i = 0; i < size; ++i) 
         free(*(arr + i));
     free(arr);
